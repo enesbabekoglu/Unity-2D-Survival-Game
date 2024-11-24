fb8be5f1a2123222ab1fee75da418bf3e203c8fc
Ahmet Talha Geçgelen <talha@talha-pc.home>
2024-11-24 17:53:14 +0300

    bush interaction

---
+++ Assets/Scripts/Bush/damageOnCollision.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class damageOnCollision : MonoBehaviour
+{
+    public float DPS;
+
+    private bool isCollidingPlayer = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 tempPos = transform.position;
+        tempPos.z = tempPos.y;
+        transform.position = tempPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if(isCollidingPlayer){
+            Debug.Log($"Damage given: {DPS * Time.deltaTime}");
+
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider){
+        if(collider.gameObject.CompareTag("player")){
+            Debug.Log($"Collision with bush: {collider.gameObject.name}");
+            isCollidingPlayer = true;
+        }
+    }
+
+
+    void OnTriggerExit2D(Collider2D collider){
+        if(collider.gameObject.CompareTag("player")){
+            Debug.Log($"No more collision with bush: {collider.gameObject.name}");
+            isCollidingPlayer = false;
+
+        }
+        
+        
+    }
+}
